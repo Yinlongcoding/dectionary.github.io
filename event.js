67deef4dabf3c35ab1010989eaa2c1576919b8c6
Sliver <wnhoper@gmail.com>
2017-03-22 13:45:48 +0800

    aside

--- event.js
+++ event.js
@@ -1,15 +1,7 @@
 $(function(){
 	$('.defAm').click(function(){$(this)[0].querySelector('audio').play()})
 	$('.defEn').click(function(){$(this)[0].querySelector('audio').play()})
-	$('.wordInput').keyup(function(event) {
-		if(event.keyCode === 13) {
-			$('.hide').hide()
-		}
-	})
-	if( $('main').css('display') === 'none' ){
-			$('.hide').show()
-	}else {
-		 $('.hide').hide()
-	}
+	$('.wordInput').keyup(function(event) {if(event.keyCode === 13) {$('.hide').hide()}})
+	if( $('main').css('display') === 'none' ){$('.hide').show()}else {$('.hide').hide()}
 })
 
