f80f23c60babaec9e9e05b1725114c617838516c
Sliver <wnhoper@gmail.com>
2017-03-22 23:16:47 +0800

    13

--- event.js
+++ event.js
@@ -4,18 +4,24 @@ $(function(){
 	$('.defAm').click(function(){$(this)[0].querySelector('audio').play()})
 	$('.defEn').click(function(){$(this)[0].querySelector('audio').play()})
 	$('.wordInput').keyup(function(event) {if(event.keyCode === 13) {$('.hide').hide()}})
-	$('.aside-nav li').click(function(){
-		$(this).css('opacity','1')
-		$(this).siblings().css('opacity','0.2')
-	})
-	$('.sunning').click(function(){
-		$('body').css('background','#fff').css('opacity','1')
-	})
-
+	$('.aside-nav li').click(function(){$(this).css('opacity','1'); $(this).siblings().css('opacity','0.2') })
 	$('.night').click(function(){
-		$('body').css('background','#303030').css('opacity','0.5')
-		$('header').css('color','#b0b0b0')
+		var style = document.createElement('style')
+			style.innerHTML = `
+				body { background: #303030;  opacity: 0.5; }
+				header { color: #b0b0b0;}
+				.wordcontents h1 { line-height: 3rem; background: -webkit-linear-gradient(left, #42b983, #303030); background: -o-linear-gradient(left, #42b983, #303030);background: linear-gradient(to right, #42b983, #303030);color: #fff;text-indent: 2rem;}
+				.wordcontents .pron i { color: #fff;}
+				.wordcontents .def { color: #fff;}
+				.wordcontents .mean { color: #FFD39B;}
+				.wordcontents .example { color: #f2f2f2; }
+				.history { color: #fff; }
+				.history ul li i { background-color: inherit; }`
+		
+		$('head')[0].appendChild(style)
 	})
 
+	$('.sunning').click(function(){
+		$('head style').remove()	
+	})
 })
-
