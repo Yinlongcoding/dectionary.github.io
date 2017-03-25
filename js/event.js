6058c79baff9892514650d09e5b8692f2b75f24e
Sliver <wnhoper@gmail.com>
2017-03-25 23:50:36 +0800

    17

--- js/event.js
+++ js/event.js
@@ -1,7 +1,6 @@
 $(function(){
-	if( $('main').css('display') === 'none' ){$('.hide').show()}else {$('.hide').hide()}
-
-	$('.close').click(function(){$('.hide').hide()})
+	if( $('main').css('display') === 'none' ){$('.hide').fadeIn(1000)} else {$('.hide').hide()}
+	$('.close').click(function(){$('.hide').fadeOut(600)})
 	$('.defAm').click(function(){$(this)[0].querySelector('audio').play()})
 	$('.defEn').click(function(){$(this)[0].querySelector('audio').play()})
 	$('.wordInput').keyup(function(event) {if(event.keyCode === 13) {$('.hide').hide()}})
@@ -9,7 +8,7 @@ $(function(){
 	$('.night').click(function(){
 		var style = document.createElement('style')
 			style.innerHTML = `
-				body { background: #000;}
+		body { background: #000;}
         header { color: #062f4f;}
         header .searchInput { border-color: #062f4f;}
         header .searchInput input {  color: #c0c0c0; }
@@ -28,10 +27,10 @@ $(function(){
         .history { color: #c0c0c0; }
         .history h1 i {color: #062f4f;}
         .history ul li i { background-color: inherit; }
-        .aside .aside-nav .night i { text-shadow: 0rem 0rem 2rem #a2abd3; }`
+        .aside .aside-nav .night i { text-shadow: 0rem 0rem 2rem #a2abd3; }
+        .hide .close { color: #b0b0b0;}`
 						
-		$('head')[0].appendChild(style)
-	})
+		$('head')[0].appendChild(style)})
 	$('.sunning').click(function(){$('head style').remove()	})
 	$(window).scroll(function(){ if($(window).scrollTop() >= 300) {$('.toupper').fadeIn(300) }else{ $('.toupper').fadeOut(300)}})
     $('.toupper').click(function(){$('html,body').animate({scrollTop: '0px'}, 800)})
