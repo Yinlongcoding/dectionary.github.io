bcde730cf208440d59d163abe33047f4de984a96
Sliver <wnhoper@gmail.com>
2017-03-27 11:27:19 +0800

    18

--- js/event.js
+++ js/event.js
@@ -9,25 +9,24 @@ $(function(){
 		var style = document.createElement('style')
 			style.innerHTML = `
 		body { background: #000;}
-        header { color: #062f4f;}
-        header .searchInput { border-color:#062f4f;}
-        header .searchInput input {  color: #c0c0c0; }
-        header .searchInput .searchword { background-color: #062f4f;color: #b0b0b0; }
-        header .searchInput .searchword i { color: #b0b0b0; }
+        header { color: #336699;}
+        header .searchInput { border-color: #336699;}
+        header .searchInput input {  color: #fbfbfb; }
+        header .searchInput .searchword { background:#336699;  color:#fbfbfb; }
         .wordcontents h1 { 
-        background: -webkit-linear-gradient(left,#062f4f, #062f4f, #000); 
-        background: -o-linear-gradient(left,#062f4f, #062f4f, #000);
-        background: linear-gradient(to right,#062f4f, #062f4f, #000);
-        color: #c0c0c0;}
+        background: -webkit-linear-gradient(left,#336699, #000); 
+        background: -o-linear-gradient(left,#336699, #000);
+        background: linear-gradient(to right,#336699, #000);
+        color: #fbfbfb;}
         .wordcontents .pron .country {color:#6b7a8f ;}
         .wordcontents .pron i { color:#6b7a8f;}
         .wordcontents .def { color: #b0b0b0;}
-        .wordcontents .def i{ color: #EEC591;}       
-        .wordcontents .example { color: #b1b1b1; }
-        .history { color: #c0c0c0; }
-        .history h1 i {color: #062f4f;}
+        .wordcontents .def i{ color: #EEC591;}  
+        .wordcontents .mean {color: #ffcccc;}     
+        .wordcontents .example { color: #fbfbfb; }
+        .history { color: #fbfbfb; }
+        .history h1 i {color: #336699;}
         .history ul li i { background-color: inherit; }
-        .aside .aside-nav .night i { text-shadow: 0rem 0rem 2rem #a2abd3; }
         .hide .close { color: #b0b0b0;}`
 						
 		$('head')[0].appendChild(style)})
